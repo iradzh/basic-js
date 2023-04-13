e4bbcc0d08d4bb5116c371b3f068edd7eac2336b
Iryna Zhebryk <irinazhebrik@gmail.com>
2023-04-13 15:46:19 +0200

    feat: +2 solved

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given matrix, a rectangular matrix of integers,
@@ -16,11 +16,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getMatrixElementsSum(matrix) {
+  let newArr = transpose(matrix);
+  let result = [];
+  let sum = 0;
+
+  for (let i = 0; i < newArr.length; i++) {
+    for (let j = 0; j < newArr[i].length; j++) {
+      if (newArr[i][j - 1] !== 0) {
+        result.push(newArr[i][j]);
+      }
+    }
+  }
+  for (let index = 0; index < result.length; index++) {
+    sum += result[index];
+  }
+  return sum;
 }
 
 module.exports = {
-  getMatrixElementsSum
+  getMatrixElementsSum,
 };
+
+function transpose(matrix) {
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+
+  const result = [];
+  for (let j = 0; j < cols; j++) {
+    result[j] = [];
+    for (let i = 0; i < rows; i++) {
+      result[j][i] = matrix[i][j];
+    }
+  }
+  return result;
+}
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array with heights, sort them except if the value is -1.
@@ -11,11 +11,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  let arrWithEls = [];
+  for (let index = 0; index < arr.length; index++) {
+    if (arr[index] !== -1) {
+      arrWithEls.push(arr[index]);
+      arr[index] = 0;
+    }
+  }
+
+  arrWithEls.sort((a, b) => a - b);
+
+  for (let index = 0; index < arr.length; index++) {
+    if (arr[index] == 0) {
+      arr[index] = arrWithEls.shift();
+    }
+  }
+  return arr;
 }
 
 module.exports = {
-  sortByHeight
+  sortByHeight,
 };
