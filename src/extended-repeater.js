f2fa5e67b2cde3215d45547a62676f86fe713fd8
Iryna Zhebryk <irinazhebrik@gmail.com>
2023-04-14 18:34:46 +0200

    feat: solved +1

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,74 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let string;
+
+  if (str == null) {
+    string = "null";
+  } else {
+    string = `${str}`.toString();
+  }
+
+  if (typeof options.addition == "undefined") {
+    cleanAdd = "";
+  } else if (options.addition == null) {
+    cleanAdd = "null";
+  } else {
+    cleanAdd = `${options.addition}`.toString();
+  }
+
+  let repeated = "";
+  let adds = "";
+
+  if (!options.separator) {
+    options.separator = "+";
+  }
+  if (!options.additionSeparator) {
+    options.additionSeparator = "|";
+  }
+
+  if (!options.additionRepeatTimes) {
+    adds += cleanAdd;
+  } else {
+    for (let index = 0; index < options.additionRepeatTimes; index++) {
+      if (index == 0) {
+        adds += cleanAdd;
+      } else {
+        adds += options.additionSeparator + cleanAdd;
+      }
+    }
+  }
+
+  if (!options.repeatTimes) {
+    return string + `${options.addition}`;
+  } else {
+    for (let index = 0; index < options.repeatTimes; index++) {
+      if (index == 0) {
+        repeated += string + adds;
+      } else {
+        repeated += options.separator + string + adds;
+      }
+    }
+
+    return repeated;
+  }
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
