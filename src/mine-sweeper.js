36acac4e37391505ad3652002c66060df33443f4
Iryna Zhebryk <irinazhebrik@gmail.com>
2023-04-14 19:37:37 +0200

    feat: solved +1

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,69 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let result = [];
+  for (let row = 0; row < matrix.length; row++) {
+    let line = [];
+    for (let col = 0; col < matrix[row].length; col++) {
+      const el = matrix[row][col];
+      let counter = 0;
+      counter += checkTopLeft(matrix, row, col) ? 1 : 0;
+      counter += checkTop(matrix, row, col) ? 1 : 0;
+      counter += checkTopRight(matrix, row, col) ? 1 : 0;
+
+      counter += checkLeft(matrix, row, col) ? 1 : 0;
+      counter += checkRight(matrix, row, col) ? 1 : 0;
+
+      counter += checkBottomLeft(matrix, row, col) ? 1 : 0;
+      counter += checkBottom(matrix, row, col) ? 1 : 0;
+      counter += checkBottomRight(matrix, row, col) ? 1 : 0;
+      line.push(counter);
+    }
+    result.push(line);
+  }
+  return result;
+}
+
+function checkTopLeft(matrix, row, col) {
+  return col - 1 >= 0 && row - 1 >= 0 && matrix[row - 1][col - 1] == true;
+}
+
+function checkTop(matrix, row, col) {
+  return row - 1 >= 0 && matrix[row - 1][col] == true;
+}
+
+function checkTopRight(matrix, row, col) {
+  return (
+    col + 1 < matrix.length && row - 1 >= 0 && matrix[row - 1][col + 1] == true
+  );
+}
+
+function checkLeft(matrix, row, col) {
+  return col - 1 >= 0 && matrix[row][col - 1] == true;
+}
+function checkRight(matrix, row, col) {
+  return col + 1 < matrix.length && matrix[row][col + 1] == true;
+}
+
+function checkBottomLeft(matrix, row, col) {
+  return (
+    col - 1 >= 0 && row + 1 < matrix.length && matrix[row + 1][col - 1] == true
+  );
+}
+
+function checkBottom(matrix, row, col) {
+  return row + 1 < matrix.length && matrix[row + 1][col] == true;
+}
+
+function checkBottomRight(matrix, row, col) {
+  return (
+    col + 1 < matrix.length &&
+    row + 1 < matrix.length &&
+    matrix[row + 1][col + 1] == true
+  );
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
